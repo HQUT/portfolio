896e6a80e4d1f8d833705b54ec7032ebe549ac71
Hiba Qutbuddin Habib <126714611+HQUT@users.noreply.github.com>
2025-02-09 19:17:11 +0100

    Create custom.js

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+// Dark Mode Toggle
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+// Toggle Contact Information
+function toggleContact() {
+  document.getElementById("contact-info").classList.toggle("show");
+}
